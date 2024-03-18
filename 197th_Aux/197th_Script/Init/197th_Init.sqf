e87057d97a491e824ce766def0370beb3d3015b9
Bragg <maxbrochetpc@gmail.com>
2024-03-18 12:46:17 +0100

    Fixe Earplug

--- 197th_Aux/197th_Script/Init/197th_Init.sqf
+++ 197th_Aux/197th_Script/Init/197th_Init.sqf
@@ -15,9 +15,6 @@
 //param FortifyTools
 [] execVM "197th_Script\Init\FortifyTools.sqf";
 
-//Add Earplugs
-[] execVM "197th_Script\Init\Earplugs.sqf";
-
 // Cinematique
 ["197th_Script\Data\DebutMission.ogv"] call BIS_fnc_quotations;
 ["<t color='#ffffff' size='.5'>Bienvenue à bord de la 197th Compagnie Chimaera !<br />Vous avez déjà vu l'introduction ?<br />Appuyez sur la touche [Espace] pour la passer.</t>", -1, 1, 6, 1, 0, 789] spawn BIS_fnc_dynamicText;
--- 197th_Aux/197th_Script/Init/CfgVehicles_Earplugs.hpp
+++ 197th_Aux/197th_Script/Init/CfgVehicles_Earplugs.hpp
@@ -1,21 +1,24 @@
 class Man;
 class CAManBase: Man {
+    class EventHandlers {
+        class 197thEarplugs {
+            init = "_this execVM '197th_Script\init\Earplugs.sqf';";
+        };
+    };
     class ACE_SelfActions {
-        class ACE_Equipment {
-            class PutInEarplugs {
-                displayName = "Enclencher les filtres audio";
-                condition = "NreEarplugsActive == 0 && '197th_Helmet' in headgear _player  && (isNil 'NreEarplugsActive') == false";
-                exceptions[] = {};
-                statement = "[_player] execVM '197th_Script\Exec\Earplugs.sqf'";
-                icon = "197th_Script\Data\waves.paa";
-			};
-            class PutOutEarplugs {
-                displayName = "Desactiver les filtres audio";
-                condition = "NreEarplugsActive == 1 && (isNil 'NreEarplugsActive') == false";
-                exceptions[] = {};
-                statement = "[_player] execVM '197th_Script\Exec\Earplugs.sqf'";
-                icon = "197th_Script\Data\waves.paa";
-			};
+        class PutInEarplugs {
+            displayName = "Enclencher les filtres audio";
+            condition = "NreEarplugsActive == 0 && (isNil 'NreEarplugsActive') == false";
+            exceptions[] = {};
+            statement = "[_player] execVM '197th_Script\Exec\Earplugs.sqf'";
+            icon = "197th_Script\Data\waves.paa";
+	    };
+        class PutOutEarplugs {
+            displayName = "Desactiver les filtres audio";
+            condition = "NreEarplugsActive == 1 && (isNil 'NreEarplugsActive') == false";
+            exceptions[] = {};
+            statement = "[_player] execVM '197th_Script\Exec\Earplugs.sqf'";
+            icon = "197th_Script\Data\unwaves.paa";
 		};
 	};
 };
