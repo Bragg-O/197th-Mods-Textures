668e085ee957f092d1dbc07465ebe5e9d2cf16c5
Bragg <maxbrochetpc@gmail.com>
2024-04-03 19:46:38 +0200

    Add P2 Helmet Damos

--- 197th_Aux/197th_Equipment/Config.cpp
+++ 197th_Aux/197th_Equipment/Config.cpp
@@ -69,6 +69,7 @@ class CfgWeapons {
         Helmet_197th_XX(Customs,Elektro);
         Helmet_197th_XX(Customs,Launcher);
         Helmet_197th_XX(Customs,Vanguard);
+        Helmet_197th_XX(Customs,Damos);
     //HelmetVisorLight_197th_XX(type,name) -> 197th_Helmet_##type##_##name##
         HelmetVisorLight_197th_XX(Customs,Bragg);
     //HelmetPilot_197th_XX(type,name) -> 197th_HelmetPilot_##type##_##name##
@@ -165,6 +166,7 @@ class CfgVehicles {
         HeadgearItem_197th_XX(Customs,Elektro);
         HeadgearItem_197th_XX(Customs,Launcher);
         HeadgearItem_197th_XX(Customs,Vanguard);
+        HeadgearItem_197th_XX(Customs,Damos);
     //HeadgearItemPilot_197th_XX(type,name) -> 197th_HeadgearPilot_##type##_##name##
         HeadgearItemPilot_197th_XX(Regs,Pilot);
     //HeadgearItemARF_197th_XX(type,name) -> 197th_HeadgearARF_##type##_##name##
