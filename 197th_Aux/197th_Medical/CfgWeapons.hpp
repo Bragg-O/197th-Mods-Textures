143bcefc27f2e1ff0a7d736466228f9da7a951b1
Bragg <maxbrochetpc@gmail.com>
2024-04-06 18:56:13 +0200

    Update Desc Item Medical

--- 197th_Aux/197th_Medical/CfgWeapons.hpp
+++ 197th_Aux/197th_Medical/CfgWeapons.hpp
@@ -9,9 +9,9 @@ class CfgWeapons {
 	class ACE_epinephrine;
 	class ACE_adenosine;
 	class ACE_morphine;
-	MedicalItem_197th_XX(Injecteur (Epinephrine),epinephrine,"Dose d'epinephrine.<br/>Frequence cardiaque : ++++<br/>Dose Letal : 10");
-	MedicalItem_197th_XX(Injecteur (Adenosine),adenosine,"Dose d'adenosine.<br/>Frequence cardiaque : --<br/>Dose Letal : 6");
-	MedicalItem_197th_XX(Injecteur (Morphine),morphine,"Dose de morphine.<br/>Frequence cardiaque : ---<br/>Douleur : ---<br/>Viscosite : --<br/>Dose Letal : 4");
+	MedicalItem_197th_XX(Injecteur (Epinephrine),epinephrine,"Dose d'epinephrine.<br/>Frequence cardiaque : + + + +<br/>Dose Letal : 10");
+	MedicalItem_197th_XX(Injecteur (Adenosine),adenosine,"Dose d'adenosine.<br/>Frequence cardiaque : - -<br/>Dose Letal : 6");
+	MedicalItem_197th_XX(Injecteur (Morphine),morphine,"Dose de morphine.<br/>Frequence cardiaque : - - -<br/>Douleur : - - -<br/>Viscosite : - -<br/>Dose Letal : 4");
 
 	class ACE_packingBandage;
 	class ACE_elasticBandage;
@@ -30,14 +30,14 @@ class CfgWeapons {
 	class ACE_salineIV;
 	class ACE_salineIV_250;
 	class ACE_salineIV_500;
-	MedicalItem_197th_XX(IV (Saline 1000ml),salineIV,"Transfusion IV pouvant etre utilisee pour compenser les pertes de sang faibles a moderees.<br/>Debit IV : +++");
-	MedicalItem_197th_XX(IV (Saline 250ml),salineIV_250,"Transfusion IV pouvant etre utilisee pour compenser les pertes de sang faibles a moderees.<br/>Debit IV : +++");
-	MedicalItem_197th_XX(IV (Saline 500ml),salineIV_500,"Transfusion IV pouvant etre utilisee pour compenser les pertes de sang faibles a moderees.<br/>Debit IV : +++");
+	MedicalItem_197th_XX(IV (Saline 1000ml),salineIV,"Transfusion IV pouvant etre utilisee pour compenser les pertes de sang faibles a moderees.<br/>Debit IV : + + +");
+	MedicalItem_197th_XX(IV (Saline 250ml),salineIV_250,"Transfusion IV pouvant etre utilisee pour compenser les pertes de sang faibles a moderees.<br/>Debit IV : + + +");
+	MedicalItem_197th_XX(IV (Saline 500ml),salineIV_500,"Transfusion IV pouvant etre utilisee pour compenser les pertes de sang faibles a moderees.<br/>Debit IV : + + +");
 	
 	class ACE_plasmaIV;
 	class ACE_plasmaIV_250;
 	class ACE_plasmaIV_500;
-	MedicalItem_197th_XX(IV (Plasma 1000ml),plasmaIV,"Transfusion IV pouvant etre utilisee des qu'il y a un minimum de sang.<br/>Debit IV : ++");
-	MedicalItem_197th_XX(IV (Plasma 250ml),plasmaIV_250,"Transfusion IV pouvant etre utilisee des qu'il y a un minimum de sang.<br/>Debit IV : ++");
-	MedicalItem_197th_XX(IV (Plasma 500ml),plasmaIV_500,"Transfusion IV pouvant etre utilisee des qu'il y a un minimum de sang.<br/>Debit IV : ++");
+	MedicalItem_197th_XX(IV (Plasma 1000ml),plasmaIV,"Transfusion IV pouvant etre utilisee des qu'il y a un minimum de sang.<br/>Debit IV : + +");
+	MedicalItem_197th_XX(IV (Plasma 250ml),plasmaIV_250,"Transfusion IV pouvant etre utilisee des qu'il y a un minimum de sang.<br/>Debit IV : + +");
+	MedicalItem_197th_XX(IV (Plasma 500ml),plasmaIV_500,"Transfusion IV pouvant etre utilisee des qu'il y a un minimum de sang.<br/>Debit IV : + +");
 };
