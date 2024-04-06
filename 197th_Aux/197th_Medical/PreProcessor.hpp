7ba39c7d20317f94f4edd243b600cecb1d70e3de
Bragg <maxbrochetpc@gmail.com>
2024-04-06 18:54:00 +0200

    Update PreProcessor.hpp

--- 197th_Aux/197th_Medical/PreProcessor.hpp
+++ 197th_Aux/197th_Medical/PreProcessor.hpp
@@ -1,7 +1,8 @@
-#define MedicalItem_197th_XX(name,type) \
+#define MedicalItem_197th_XX(name,type,desc) \
     class 197th_Medical_##type## : ACE_##type## { \
 		author = 197th Compagnie Chimaera; \
         displayName = [197th] ##name##; \
+		descriptionShort = ##desc##; \
 		picture = \197th_Medical\_ui\##type##_ca.paa; \
 		ACE_isMedicalItem = 1; \
 	}
