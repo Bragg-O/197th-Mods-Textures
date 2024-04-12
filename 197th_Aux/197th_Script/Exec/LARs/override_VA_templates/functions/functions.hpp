47f075c434f1a66418fb1e46d9789ccbeb700079
Bragg <maxbrochetpc@gmail.com>
2024-04-12 14:12:05 +0200

    ReADD LARs VA override

--- 197th_Aux/197th_Script/Exec/LARs/override_VA_templates/functions/functions.hpp
+++ 197th_Aux/197th_Script/Exec/LARs/override_VA_templates/functions/functions.hpp
@@ -3,7 +3,7 @@ class LARs_overrideVATemplates
 	tag = "LARs";
 	class Inventory
 	{
-		file = "197th_Fonctions\override_VA_templates\functions";
+		file = "197th_Script\Exec\LARs\override_VA_templates\functions";
 		class initOverride { postInit = 1; };
 		class loadInventory_whiteList {};
 		class overrideVAButtonDown {};
