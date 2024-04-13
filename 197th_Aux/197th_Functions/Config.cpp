4d6eee312b8763b63df3eecaa6e718f286c07b65
Bragg <maxbrochetpc@gmail.com>
2024-04-13 15:13:38 +0200

    Create Config.cpp

---
+++ 197th_Aux/197th_Functions/Config.cpp
@@ -0,0 +1,23 @@
+/*
+    Author: Bragg for 197th Compagnie Chimaera
+
+    Description: Please do not [Copy, Modify, Plagiarize] the following content without prior authorization from its creator.
+    
+    Website: [www.197th.fr]
+*/
+
+class CfgPatches {
+    class 197th_Functions {
+        name = "197th Functions";
+        author = "Bragg for 197th Compagnie Chimaera";
+        url = "http://www.197th.fr";
+        requiredAddons[] = {};
+        weapons[] = {};
+        units[] = {};
+    };
+};
+class CfgFunctions {
+    class CIM_197th_Functions {
+        tag = "CIM";
+    };
+};
