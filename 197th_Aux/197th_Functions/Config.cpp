d456a26a32bd00ad0a03a74b2402505147769117
Bragg <maxbrochetpc@gmail.com>
2024-04-16 14:29:57 +0200

    Add override_VA_templates to 197th_Functions

--- 197th_Aux/197th_Functions/Config.cpp
+++ 197th_Aux/197th_Functions/Config.cpp
@@ -65,4 +65,19 @@ class CfgFunctions {
             class onVehicleServiceChangePylon {};
         };
     };
+    class LARs_overrideVATemplates {
+        tag = "LARs";
+        class Inventory {
+            file = "197th_Aux\197th_Functions\LARs\override_VA_templates\functions";
+            class initOverride {
+                postInit = 1;
+            };
+            class loadInventory_whiteList {};
+            class overrideVAButtonDown {};
+            class overrideVATemplateOK {};
+            class applyLBColors {};
+            class showRestrictedItems {};
+            class addVAKeyEvents {};
+        };
+    };
 };
