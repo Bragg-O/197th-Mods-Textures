a8d26cdbccb936d427c8cbeb519a507afabb3d92
Bragg <maxbrochetpc@gmail.com>
2024-04-17 11:22:49 +0200

    Update PreProcessor.hpp

--- 197th_Aux/197th_Loadout/PreProcessor.hpp
+++ 197th_Aux/197th_Loadout/PreProcessor.hpp
@@ -4,7 +4,13 @@
         scope = 2; \
         class EventHandlers { \
             class InitLoadout197th { \
-                init = [(_this select 0),'##type##'] spawn CIM_fnc_initLoadouts; \
+                init = [(_this select 0),'##type##_197th'] spawn CIM_fnc_initLoadouts; \
             }; \
         }; \
 	}
+
+
+#define Variants_197th_XX(type,name) \
+    class ##type## { \
+        displayName = ##name##; \
+    }
