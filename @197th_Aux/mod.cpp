3c22fa07e40f096acd4fbcdedddc51d815b92a97
Bragg <maxbrochetpc@gmail.com>
2024-04-19 14:05:14 +0200

    Remove @197th_Aux form Git

--- @197th_Aux/mod.cpp
+++
@@ -1,8 +0,0 @@
-name 			= "197th Compagnie Chimaera - Auxiliary"; // Name of your mod
-author 			= "197th Compagnie Chimaera - Bragg"; // Affects Arma 3 Launcher, when the mod are loaded as local
-logo 			= "logo.paa"; // Logo displayed in the main menu
-logoOver 		= "logo1.paa"; // When the mouse is over, in the main menu
-actionName 		= "Discord";
-action 			= "https://discord.gg/qQs5GnV3Mt"; // Website URL, that can be accessed from the expansions menu
-overview 		= "Mods de la communautés 197th Compagnie Chimaera"; // Supports structured text
-logoSmall 		= "logo.paa"; // Display in creative lists, next to the entities added by the mod
