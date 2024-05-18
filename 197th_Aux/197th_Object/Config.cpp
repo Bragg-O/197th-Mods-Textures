8f195b0c52aad77a1cb2e7d87d029be17756f062
Bragg <maxbrochetpc@gmail.com>
2024-05-18 15:19:19 +0200

    Add 197th_SupplySlingload_Fuel

--- 197th_Aux/197th_Object/Config.cpp
+++ 197th_Aux/197th_Object/Config.cpp
@@ -16,6 +16,7 @@ class CfgPatches {
         requiredAddons[] = {"197th_weapon","197th_Equipment"};
         weapons[] = {};
         units[] = { //All for curator
+            "197th_SupplySlingload_Fuel",
             "197th_SupplySlingload_Repair",
             "197th_Supply_Empty",
             "197th_Supply_Ammo",
