f06ae0e61699ec3d5f6a8202ca82eac3a5321394
Bragg <maxbrochetpc@gmail.com>
2024-05-29 16:50:20 +0200

    Add P2 Skibur Helmet

--- 197th_Aux/197th_Equipment/Config.cpp
+++ 197th_Aux/197th_Equipment/Config.cpp
@@ -50,6 +50,7 @@ class CfgPatches {
             CfgPatches_XX(Headgear,Customs,Doc),
             CfgPatches_XX(Headgear,Customs,Tesler),
             CfgPatches_XX(Headgear,Customs,Blast),
+            CfgPatches_XX(Headgear,Customs,Skibur),
             CfgPatches_XX(HeadgearPilot,Regs,Pilot),
             CfgPatches_XX(HeadgearARF,Regs,ARF),
             CfgPatches_XX(HeadgearBARC,Regs,BARC),
@@ -91,6 +92,7 @@ class CfgWeapons {
         Helmet_197th_XX(Customs,Doc);
         Helmet_197th_XX(Customs,Tesler);
         Helmet_197th_XX(Customs,Blast);
+        Helmet_197th_XX(Customs,Skibur);
     //HelmetVisorLight_197th_XX(type,name) -> 197th_Helmet_##type##_##name##
         HelmetVisorLight_197th_XX(Customs,Bragg);
     //HelmetPilot_197th_XX(type,name) -> 197th_HelmetPilot_##type##_##name##
@@ -196,6 +198,7 @@ class CfgVehicles {
         HeadgearItem_197th_XX(Customs,Doc);
         HeadgearItem_197th_XX(Customs,Tesler);
         HeadgearItem_197th_XX(Customs,Blast);
+        HeadgearItem_197th_XX(Customs,Skibur);
     //HeadgearItemPilot_197th_XX(type,name) -> 197th_HeadgearPilot_##type##_##name##
         HeadgearItemPilot_197th_XX(Regs,Pilot);
     //HeadgearItemARF_197th_XX(type,name) -> 197th_HeadgearARF_##type##_##name##
