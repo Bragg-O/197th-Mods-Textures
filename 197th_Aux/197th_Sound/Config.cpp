e68e3d1157952c94646a82486d4877fd9fbf12c2
Bragg <maxbrochetpc@gmail.com>
2024-05-30 20:34:49 +0200

    Add 197th_Sound

---
+++ 197th_Aux/197th_Sound/Config.cpp
@@ -0,0 +1,22 @@
+/*
+    Author: Bragg for 197th Compagnie Chimaera
+
+    Description: Please do not [Copy, Modify, Plagiarize] the following content without prior authorization from its creator.
+    
+    Website: [www.197th.fr]
+*/
+
+class CfgPatches {
+    class 197th_Sound {
+        name = "197th Sound";
+        author = "Bragg for 197th Compagnie Chimaera";
+        url = "http://www.197th.fr";
+        requiredAddons[] = {};
+        units[] = {};
+        weapons[] = {};
+    };
+};
+
+class CfgSounds
+{
+};
