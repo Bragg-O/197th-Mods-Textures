6bc30363439aa73d8c2eb9f5c6562c1a0ab91cca
Bragg <maxbrochetpc@gmail.com>
2024-10-06 19:50:01 +0200

    Update Loadout

--- CIM_Aux/197th_Regs/Loadout/CfgVehicles.hpp
+++ CIM_Aux/197th_Regs/Loadout/CfgVehicles.hpp
@@ -1,23 +1,192 @@
-class 197th_Base_Loadout : 197th_Unit
+class 197th_AA_Loadout : 197th_Unit
 {
 	author = "[CIM] Bragg";
-	displayName = "[197th] Base (Loadout)";
-	scope = 0;
-	scopecurator = 0;
+	displayName = "[197th] AA (Loadout)";
+	scope = 2;
 	side = 3;
 	editorSubcategory = "CIM_Loadout";
 	faction = "197th";
+	class EventHandlers
+	{
+		class InitLoadout197th
+		{
+			init = [ (_this select 0), 'AA_197th' ] call CIM_fnc_initLoadout;
+		};
+	};
+	uniformClass = "197th_Uniform_Regs_Trooper";
+	backpack = "";
+	weapons[] = {"Throw", "Put"};
+	magazines[] = {"SWLW_beltGrenade_mag"};
+	items[] = {"FirstAidKit", "FirstAidKit", "FirstAidKit"};
+	linkedItems[] = {"197th_Vest_Regs_0", "197th_Helmet_Regs_Trooper", "ItemMap", "ItemCompass", "ItemWatch", "JLTS_clone_comlink"};
 };
 
-Loadout_197th_XX(AA);
-Loadout_197th_XX(ARC);
-Loadout_197th_XX(ARF);
-Loadout_197th_XX(AT);
-Loadout_197th_XX(Fantassin);
-Loadout_197th_XX(Grenadier);
-Loadout_197th_XX(Heavy);
-Loadout_197th_XX(Medic);
-Loadout_197th_XX(Pilote);
-Loadout_197th_XX(Radioman);
-Loadout_197th_XX(Sapeur);
-Loadout_197th_XX(Shield);
+class 197th_ARC_Loadout : 197th_Unit
+{
+	author = "[CIM] Bragg";
+	displayName = "[197th] ARC (Loadout)";
+	scope = 2;
+	side = 3;
+	editorSubcategory = "CIM_Loadout";
+	faction = "197th";
+	class EventHandlers
+	{
+		class InitLoadout197th
+		{
+			init = [ (_this select 0), 'ARC_197th' ] call CIM_fnc_initLoadout;
+		};
+	};
+	uniformClass = "197th_Uniform_Regs_Trooper";
+	backpack = "";
+	weapons[] = {"Throw", "Put"};
+	magazines[] = {"SWLW_beltGrenade_mag"};
+	items[] = {"FirstAidKit", "FirstAidKit", "FirstAidKit"};
+	linkedItems[] = {"197th_Vest_Regs_ARC", "197th_Helmet_Regs_Trooper", "ItemMap", "ItemCompass", "ItemWatch", "JLTS_clone_comlink"};
+};
+
+class 197th_Medic_Loadout : 197th_Unit
+{
+	author = "[CIM] Bragg";
+	displayName = "[197th] Medic (Loadout)";
+	scope = 2;
+	side = 3;
+	editorSubcategory = "CIM_Loadout";
+	faction = "197th";
+	class EventHandlers
+	{
+		class InitLoadout197th
+		{
+			init = [ (_this select 0), 'Medic_197th' ] call CIM_fnc_initLoadout;
+		};
+	};
+	uniformClass = "197th_Uniform_Regs_Medecin";
+	backpack = "";
+	weapons[] = {"Throw", "Put"};
+	magazines[] = {"SWLW_beltGrenade_mag"};
+	items[] = {"FirstAidKit", "FirstAidKit", "FirstAidKit"};
+	linkedItems[] = {"197th_Vest_Regs_Medical", "197th_Helmet_Regs_Trooper", "ItemMap", "ItemCompass", "ItemWatch", "JLTS_clone_comlink"};
+};
+
+class 197th_ARF_Loadout : 197th_Unit
+{
+	author = "[CIM] Bragg";
+	displayName = "[197th] ARF (Loadout)";
+	scope = 2;
+	side = 3;
+	editorSubcategory = "CIM_Loadout";
+	faction = "197th";
+	class EventHandlers
+	{
+		class InitLoadout197th
+		{
+			init = [ (_this select 0), 'ARF_197th' ] call CIM_fnc_initLoadout;
+		};
+	};
+	uniformClass = "197th_Uniform_Regs_Trooper";
+	backpack = "";
+	weapons[] = {"Throw", "Put"};
+	magazines[] = {"SWLW_beltGrenade_mag"};
+	items[] = {"FirstAidKit", "FirstAidKit", "FirstAidKit"};
+	linkedItems[] = {"197th_Vest_Regs_0", "197th_HelmetARF_Regs_ARF", "ItemMap", "ItemCompass", "ItemWatch", "JLTS_clone_comlink"};
+};
+
+class 197th_Pilote_Loadout : 197th_Unit
+{
+	author = "[CIM] Bragg";
+	displayName = "[197th] Pilote (Loadout)";
+	scope = 2;
+	side = 3;
+	editorSubcategory = "CIM_Loadout";
+	faction = "197th";
+	class EventHandlers
+	{
+		class InitLoadout197th
+		{
+			init = [ (_this select 0), 'Pilote_197th' ] call CIM_fnc_initLoadout;
+		};
+	};
+	uniformClass = "197th_Uniform_Regs_Trooper";
+	backpack = "";
+	weapons[] = {"Throw", "Put"};
+	magazines[] = {"SWLW_beltGrenade_mag"};
+	items[] = {"FirstAidKit", "FirstAidKit", "FirstAidKit"};
+	linkedItems[] = {"197th_Vest_Regs_0", "197th_HelmetPilot_Regs_Pilot", "ItemMap", "ItemCompass", "ItemWatch", "JLTS_clone_comlink"};
+};
+
+class 197th_AT_Loadout : 197th_AA_Loadout
+{
+	author = "[CIM] Bragg";
+	displayName = "[197th] AT (Loadout)";
+	class EventHandlers
+	{
+		class InitLoadout197th
+		{
+			init = [ (_this select 0), 'AT_197th' ] call CIM_fnc_initLoadout;
+		};
+	};
+};
+
+class 197th_Fantassin_Loadout : 197th_AA_Loadout
+{
+	author = "[CIM] Bragg";
+	displayName = "[197th] Fantassin (Loadout)";
+	class EventHandlers
+	{
+		class InitLoadout197th
+		{
+			init = [ (_this select 0), 'Fantassin_197th' ] call CIM_fnc_initLoadout;
+		};
+	};
+};
+
+class 197th_Heavy_Loadout : 197th_AA_Loadout
+{
+	author = "[CIM] Bragg";
+	displayName = "[197th] Heavy (Loadout)";
+	class EventHandlers
+	{
+		class InitLoadout197th
+		{
+			init = [ (_this select 0), 'Heavy_197th' ] call CIM_fnc_initLoadout;
+		};
+	};
+};
+
+class 197th_Radioman_Loadout : 197th_AA_Loadout
+{
+	author = "[CIM] Bragg";
+	displayName = "[197th] Radioman (Loadout)";
+	class EventHandlers
+	{
+		class InitLoadout197th
+		{
+			init = [ (_this select 0), 'Radioman_197th' ] call CIM_fnc_initLoadout;
+		};
+	};
+};
+
+class 197th_Mortier_Loadout : 197th_AA_Loadout
+{
+	author = "[CIM] Bragg";
+	displayName = "[197th] Mortier (Loadout)";
+	class EventHandlers
+	{
+		class InitLoadout197th
+		{
+			init = [ (_this select 0), 'Mortier_197th' ] call CIM_fnc_initLoadout;
+		};
+	};
+};
+
+class 197th_Sapeur_Loadout : 197th_AA_Loadout
+{
+	author = "[CIM] Bragg";
+	displayName = "[197th] Sapeur (Loadout)";
+	class EventHandlers
+	{
+		class InitLoadout197th
+		{
+			init = [ (_this select 0), 'Sapeur_197th' ] call CIM_fnc_initLoadout;
+		};
+	};
+};
--- CIM_Aux/197th_Regs/Loadout/Data/Mortier/DC15S/Loadout.sqf
+++ CIM_Aux/197th_Regs/Loadout/Data/Mortier/DC15S/Loadout.sqf
@@ -23,28 +23,25 @@ _units addHandgunItem "197th_DC17SA_mag";
 
 comment "Add containers";
 _units forceAddUniform "197th_Uniform_Regs_Trooper";
-_units addVest "197th_Vest_Regs_4";
-_units addBackpack "197th_Backpack_Regs_Complete";
+_units addVest "197th_Vest_Regs_Holster";
+_units addBackpack "197th_Mortar_Bag";
 
 comment "Add items to containers";
-
 for "_i" from 1 to 15 do {_units addItemToUniform "197th_Medical_elasticBandage";};
 for "_i" from 1 to 15 do {_units addItemToUniform "197th_Medical_packingBandage";};
 for "_i" from 1 to 5 do {_units addItemToUniform "ACE_CableTie";};
 _units addItemToUniform "ACE_EntrenchingTool";
 _units addItemToUniform "ACE_Flashlight_XL50";
-_units addItemToUniform "197th_Medical_painkillers";
 _units addItemToUniform "197th_Medical_splint";
 for "_i" from 1 to 4 do {_units addItemToUniform "ACE_tourniquet";};
 for "_i" from 1 to 2 do {_units addItemToUniform "197th_Medical_plasmaIV_250";};
+_units addItemToUniform "197th_Medical_painkillers";
 _units addItemToVest "197th_NVG_Regs_Headlamp";
 for "_i" from 1 to 3 do {_units addItemToVest "197th_DC17SA_mag";};
 for "_i" from 1 to 5 do {_units addItemToVest "3AS_SmokeWhite";};
 for "_i" from 1 to 3 do {_units addItemToVest "3AS_ThermalDetonator";};
 for "_i" from 1 to 10 do {_units addItemToVest "197th_DC15S_mag";};
 for "_i" from 1 to 2 do {_units addItemToVest "JLTS_stun_mag_short";};
-for "_i" from 1 to 3 do {_units addItemToBackpack "197th_riot_shield_item";};
-for "_i" from 1 to 10 do {_units addItemToBackpack "3AS_SonicDet";};
 _units addHeadgear "197th_Helmet_Regs_Trooper";
 _units addGoggles "CWHUD_P2_base";
 
@@ -54,4 +51,3 @@ _units linkItem "ItemCompass";
 _units linkItem "ACE_Altimeter";
 _units linkItem "JLTS_clone_comlink";
 _units linkItem "ItemGPS";
-
